4829d25a1e51b06ae78dc41c76c086d894da5980
Harris Lapiroff <harris@chromamine.com>
2016-12-31 14:37:05 -0500

    ESLint fixes

--- app/Board.jsx
+++ app/Board.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
 import Hex from '~/Hex'
 
 const TILE_RADIUS = 24
 const TILE_HEIGHT = TILE_RADIUS * 2
-const TILE_V_DISTANCE = TILE_HEIGHT * 3 / 4
+const TILE_V_DISTANCE = (TILE_HEIGHT * 3) / 4
 const TILE_WIDTH = Math.sqrt(3) * TILE_RADIUS
 const TILE_H_DISTANCE = TILE_WIDTH
 
@@ -11,22 +11,22 @@ class Board extends Component {
 	render() {
 		const { data } = this.props
 		// width of first row of tiles plus half a tile with for each additional row
-		const boardWidth = (data[0].length * TILE_WIDTH) + (data.length * TILE_WIDTH / 2)
+		const boardWidth = (data[0].length * TILE_WIDTH) + ((data.length * TILE_WIDTH) / 2)
 		// height of tile vert distance times number of rows plus a quarter tile height for the last bit
-		const boardHeight = data.length * TILE_V_DISTANCE + TILE_HEIGHT - TILE_V_DISTANCE
+		const boardHeight = (data.length * TILE_V_DISTANCE) + (TILE_HEIGHT - TILE_V_DISTANCE)
 		return (
 			<svg
 				width={boardWidth}
 				height={boardHeight}
 			>
-				<g transform={`translate(0 ${2 * TILE_V_DISTANCE / 3})`}>
+				<g transform={`translate(0 ${(2 * TILE_V_DISTANCE) / 3})`}>
 					{this.props.data.map((row, i) => (
-						<g transform={`translate(${TILE_H_DISTANCE * i / 2} ${TILE_V_DISTANCE * i})`}>
+						<g transform={`translate(${(TILE_H_DISTANCE * i) / 2} ${TILE_V_DISTANCE * i})`}>
 							{row.map((cell, j) => (
 								<Hex
 									key={`${i}-${j}`}
 									owner={cell}
-									center={[j * TILE_H_DISTANCE + TILE_WIDTH / 2, 0]}
+									center={[(j * TILE_H_DISTANCE) + (TILE_WIDTH / 2), 0]}
 									radius={TILE_RADIUS}
 								/>
 							))}
@@ -36,6 +36,10 @@ class Board extends Component {
 			</svg>
 		)
 	}
+}
+
+Board.propTypes = {
+	data: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number)),
 }
 
 export default Board
